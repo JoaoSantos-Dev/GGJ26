0f361f838ebc2cac14af647f5eb29516899f5375
AleProfJunior <alexandre.junior@edu.sc.senai.br>
2026-01-30 16:06:49 -0300

    Feat: camera multi-target

---
+++ Assets/Scripts/CameraSystem/CameraController.cs
@@ -0,0 +1,38 @@
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace CameraSystem
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private PlayerInputManager playerInputManager;
+        [SerializeField] private CinemachineTargetGroup targetGroup;
+        
+        private void OnEnable()
+        {
+            playerInputManager.onPlayerJoined += OnPlayerJoined;
+            playerInputManager.onPlayerLeft += OnPlayerLeft;
+        }
+
+        private void OnDisable()
+        {
+            playerInputManager.onPlayerJoined -= OnPlayerJoined;
+            playerInputManager.onPlayerLeft -= OnPlayerLeft;
+        }
+
+
+        private void OnPlayerJoined(PlayerInput playerInput)
+        {
+            print("player joined");
+            targetGroup.AddMember(playerInput.transform, 1,1);
+        }
+        private void OnPlayerLeft(PlayerInput playerInput)
+        {
+            targetGroup.RemoveMember(playerInput.transform);
+
+        }
+
+        
+    }
+}
--- Assets/Scripts/PlayerSystem/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSystem/PlayerSpawner.cs
@@ -11,7 +11,7 @@ namespace Playersystem
     {
         [SerializeField] private float spawnRadius = 5;
         private PlayerInputManager inputController;
-        public event Action<int> PlayerCountUpdate;
+        public event Action<Transform,int> PlayerCountUpdate;
         private void Awake()
         {
             inputController = GetComponent<PlayerInputManager>();
@@ -31,17 +31,17 @@ namespace Playersystem
         public void OnPlayerJoined(PlayerInput playerInput)
         {
             SetRandomPosition(playerInput.transform);
-            PlayerCountUpdate?.Invoke(playerInput.playerIndex);
+            PlayerCountUpdate?.Invoke(playerInput.transform, playerInput.playerIndex);
 
         }
 
 
         private void SetRandomPosition(Transform transform)
-        {       
+        {      
             Vector3 randomPosition = UnityEngine.Random.insideUnitCircle * spawnRadius / 2;
             randomPosition.z = randomPosition.y;
             randomPosition.y = 0;
-            transform.position = transform.position +  randomPosition;
+            transform.position = this.transform.position +  randomPosition;
             
         }
         
