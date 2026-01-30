292fdb82b387223f83c34b8ff14e0fd6c7a21201
AleProfJunior <alexandre.junior@edu.sc.senai.br>
2026-01-30 19:45:07 -0300

    update

--- Assets/TEST MASK/Entities/Enemy/EnemyController.cs
+++ Assets/TEST MASK/Entities/Enemy/EnemyController.cs
@@ -1,8 +1,28 @@
 
+using System;
+using Playersystem;
+using Unity.VisualScripting;
+using UnityEngine;
+
 public class EnemyController : EntityBase
 {
+    [SerializeField, Min(1)] private int damage = 10;
     private void Awake()
     {
         MovementHandler = new EnemyMovementHandler(transform);
+        
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if(other.TryGetComponent(out PlayerController player))
+            {
+                player.TakeDamage(damage);
+            }
+        }
     }
 }
