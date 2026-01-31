1dd4b7aa4ed292bddc700bfd6216a9d91afb4c2c
AleProfJunior <alexandre.junior@edu.sc.senai.br>
2026-01-31 19:12:54 -0300

    Feat: HUD players with outline color

--- Assets/Scripts/UI/PlayerHUD.cs
+++ Assets/Scripts/UI/PlayerHUD.cs
@@ -1,6 +1,7 @@
 using System;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,10 +9,14 @@ namespace UI
 {
     public class PlayerHUD : MonoBehaviour
     {
+        [SerializeField] private Image image_MaskIcon;
+        [SerializeField] private Image image_healthBarBG;
+        [SerializeField] private Image image_maskDurationBG;
         [SerializeField] private Image image_healthBar;
         private int maxHealth;
         private PlayerController playerController;
-
+        private Material material;
+        private static int oultlineColorPropertyID = Shader.PropertyToID("_SolidOutline");
         private void Awake()
         {
             image_healthBar.fillAmount = 0;
@@ -27,7 +32,7 @@ namespace UI
             await UniTask.Delay(200);
             image_healthBar.DOFillAmount(1, 0.2f).SetEase(Ease.OutQuad);
             this.playerController = playerController;
-            image_healthBar.color = playerController.VisualConfig.Color;
+            SetImageOutlineColor(this.playerController);
             maxHealth = playerController.Health;
             playerController.HealthChanged += OnHealthChange;
         }
@@ -36,6 +41,18 @@ namespace UI
         {
             var healthPercentage = health / (float)maxHealth;
             image_healthBar.DOFillAmount(healthPercentage, 0.5f).SetEase(Ease.OutQuad);
+        }
+
+        private void SetImageOutlineColor(PlayerController player)
+        {
+            image_healthBar.color = Color.white;
+            image_healthBarBG.color = player.VisualConfig.Color;
+            image_maskDurationBG.color = player.VisualConfig.Color;
+            
+            var maskMaterial = image_MaskIcon.material;
+            maskMaterial.SetColor(oultlineColorPropertyID, player.VisualConfig.Color);
+
+
         }
     }
 }
